5ad5f92c944e033672611fb91c0faa9fca3a5012
aphrodite0720 <aleksandr.ivanovich19970720@gmail.com>
2022-01-25 02:24:58 -0500

    final
    front and back end completion

---
+++ vue-chat-front/dist/precache-manifest.1dd3dbf009207954957b1ef0c1e06f1d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f235ebed78ec8b7b43",
+    "url": "/css/app.68fd1e22.css"
+  },
+  {
+    "revision": "b607bacdb31047ca04fa",
+    "url": "/css/login.3563982f.css"
+  },
+  {
+    "revision": "93ee2957bc33099a6ced19f2a8854cb0",
+    "url": "/img/chat-background.svg"
+  },
+  {
+    "revision": "da47a1b793e679af2a664a7a63107649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44f235ebed78ec8b7b43",
+    "url": "/js/app.55022102.js"
+  },
+  {
+    "revision": "1558d7a1a432ea8580fe",
+    "url": "/js/chat.f72ee05a.js"
+  },
+  {
+    "revision": "2b80fcede7a02fefe27e",
+    "url": "/js/chat~login.60dcf509.js"
+  },
+  {
+    "revision": "6cfc05954bea4e81a7df",
+    "url": "/js/chunk-vendors.35787a9f.js"
+  },
+  {
+    "revision": "b607bacdb31047ca04fa",
+    "url": "/js/login.a38fc160.js"
+  },
+  {
+    "revision": "f052d99b84e016a7bf206a9e8845f0b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
